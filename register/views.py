933b4356f0538bc835f9d8e392e67ed04169c109
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-07-18 08:52:55 +0530

    registration done

--- register/views.py
+++ register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Registration
+from .serializers import RegistrationSerializer
 # Create your views here.
+
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = RegistrationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 'Registration successful'})
+    return Response(serializer.errors, status=400)
--- server/settings.py
+++ server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'register',
     'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
