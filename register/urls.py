9bd7e63a57b87bd94def952114cff24e2d3f3915
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-07-18 12:15:45 +0530

    all api created

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import register_user
+from .views import register_user, admin_login, students_list, delete_student, admin_logout_view
 
 urlpatterns = [
     path('register/', register_user, name='register-user'),
+    path('admin-login/', admin_login),
+    path('students/', students_list),
+    path('students/<int:pk>/delete/', delete_student),
+    path('admin/logout/', admin_logout_view, name='admin_logout'),
 ]
--- register/views.py
+++ register/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Registration
 from .serializers import RegistrationSerializer
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from django.contrib.auth import logout
 # Create your views here.
 
 
@@ -13,3 +17,33 @@ def register_user(request):
         serializer.save()
         return Response({'message': 'Registration successful'})
     return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def admin_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(username=username, password=password)
+    if user and user.is_staff:
+        return JsonResponse({'message': 'Login successful', 'admin': True})
+    return JsonResponse({'error': 'Invalid credentials'}, status=401)
+
+@api_view(['GET'])
+def students_list(request):
+    students = Registration.objects.all()
+    serializer = RegistrationSerializer(students, many=True)
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def delete_student(request, pk):
+    try:
+        student = Registration.objects.get(id=pk)
+        student.delete()
+        return Response({'message': 'Student deleted successfully'})
+    except Registration.DoesNotExist:
+        return Response({'error': 'Student not found'}, status=404)
+    
+def admin_logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return JsonResponse({'message': 'Logged out successfully'})
+    return JsonResponse({'error': 'Invalid request'}, status=400)
